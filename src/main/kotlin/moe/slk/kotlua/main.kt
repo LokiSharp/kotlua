02f7e73353daf23c163c98b2290c3910634abe1b
LokiSharp <loki.sharp@gmail.com>
2019-06-05 15:23:51 +0800

    feat: 定义 Lua Stack

--- src/main/kotlin/moe/slk/kotlua/main.kt
+++ src/main/kotlin/moe/slk/kotlua/main.kt
@@ -1,91 +1,12 @@
 package moe.slk.kotlua
 
-import moe.slk.kotlua.binchunk.types.Prototype
-import moe.slk.kotlua.binchunk.unDump
-import moe.slk.kotlua.vm.Instruction
-import moe.slk.kotlua.vm.types.OpCode
-import java.nio.file.Files
-import java.nio.file.Paths
+import moe.slk.kotlua.debug.printBinChunk
+import moe.slk.kotlua.debug.testLuaState
 
 fun main(args: Array<String>) {
+
     if (args.isNotEmpty()) {
-        val data = Files.readAllBytes(Paths.get(args[0]))
-        val proto = unDump(data)
-        list(proto)
+        printBinChunk(args[0])
     }
-}
-
-private fun list(proto: Prototype) {
-    printHeader(proto)
-    printCode(proto)
-    printDetail(proto)
-
-    proto.protos.forEach(::list)
-}
-
-private fun printHeader(prototype: Prototype) {
-    with(prototype) {
-        val funcType = if (lineDefined > 0) "function" else "main"
-        val varargFlag = if (isVararg > 0) "+" else ""
-
-        println("$funcType <$source:$lineDefined,$lastLineDefined> (${code.size} instructions)")
-
-        println(
-            "$numParams$varargFlag params, " +
-                    "$maxStackSize slots, " +
-                    "${upvalueNames.size} upvalues, " +
-                    "${localVars.size} locals, " +
-                    "${constants.size} constants, " +
-                    "${protos.size} functions"
-        )
-    }
-}
-
-private fun printCode(prototype: Prototype) {
-    with(prototype) {
-        code.forEachIndexed { i, code ->
-            val line = if (lineInfo.isNotEmpty()) lineInfo[i].toString() else "-"
-            val inst = Instruction(code)
-            print("\t${i + 1}\t[$line]\t0x${code.formatHex(8)}\t${inst.opCode.formatString(8)}\t")
-            inst.printOperands()
-            println()
-        }
-    }
-}
-
-private fun OpCode.formatString(digits: Int) = java.lang.String.format("%-${digits}s", this)
-
-private fun Int.formatHex(digits: Int) = java.lang.String.format("%0${digits}X", this)
-
-private fun printDetail(prototype: Prototype) {
-    with(prototype) {
-        println("constants (${constants.size}):")
-        var i = 1
-        for (k in constants) {
-            println("\t${i++}\t${constantToString(k)}")
-        }
-
-        i = 0
-        println("locals (${localVars.size}):")
-        for (locVar in localVars) {
-            println("\t${i++}\t${locVar.varName}\t${locVar.startPC + 1}\t${locVar.endPC + 1}")
-        }
-
-        i = 0
-        println("upvalues (${upvalues.size}):")
-        for (upval in upvalues) {
-            val name = if (upvalueNames.isNotEmpty()) upvalueNames[i] else "-"
-            println("\t${i++}\t$name\t${upval.inStack}\t${upval.idx}")
-        }
-    }
-}
-
-private fun constantToString(k: Any?): String {
-    return if (k == null) {
-        "nil"
-    } else if (k is String) {
-        "\"" + k + "\""
-    } else {
-        k.toString()
-    }
+    testLuaState()
 }
---
+++ src/main/kotlin/moe/slk/kotlua/state/LuaValue.kt
@@ -0,0 +1,17 @@
+package moe.slk.kotlua.state
+
+import moe.slk.kotlua.api.LuaType.*
+
+fun typeOf(value: Any?) = when (value) {
+    null -> LUA_TNIL
+    is Boolean -> LUA_TBOOLEAN
+    is Long, is Double -> LUA_TNUMBER
+    is String -> LUA_TSTRING
+    else -> throw Exception("Don't support ${value::class.simpleName}")
+}
+
+fun toBoolean(value: Any?) = when (value) {
+    null -> false
+    is Boolean -> value
+    else -> true
+}

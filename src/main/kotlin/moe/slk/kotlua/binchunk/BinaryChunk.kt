0f0f48e24441f14e016637cadeec9d5ae3522480
LokiSharp <loki.sharp@gmail.com>
2019-06-06 15:53:16 +0800

    refactor: 重构 BinaryChunk

--- src/main/kotlin/moe/slk/kotlua/binchunk/BinaryChunk.kt
+++ src/main/kotlin/moe/slk/kotlua/binchunk/BinaryChunk.kt
@@ -16,8 +16,10 @@ object BinaryChunk {
     fun unDump(data: ByteArray): Prototype {
         val buf = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN)
         checkHeader(buf) // 校验函数头
-        readByte(buf) // 跳过 Upvalue
-        return readProto(buf) // 返回函数原型
+        getByte(buf) // 跳过 Upvalue
+        val proto = Prototype()
+        proto.read(buf)
+        return proto // 返回函数原型
     }
 
     /**
@@ -25,10 +27,10 @@ object BinaryChunk {
      */
     private fun checkHeader(buf: ByteBuffer) {
         when {
-            !LUA_SIGNATURE.contentEquals(readBytes(buf, 4)) -> throw RuntimeException("not a precompiled chunk!")
+            !LUA_SIGNATURE.contentEquals(getBytes(buf, 4)) -> throw RuntimeException("not a precompiled chunk!")
             buf.get().toInt() != LUAC_VERSION -> throw RuntimeException("version mismatch!")
             buf.get().toInt() != LUAC_FORMAT -> throw RuntimeException("format mismatch!")
-            !LUAC_DATA.contentEquals(readBytes(buf, 6)) -> throw RuntimeException("corrupted!")
+            !LUAC_DATA.contentEquals(getBytes(buf, 6)) -> throw RuntimeException("corrupted!")
             buf.get().toInt() != CINT_SIZE -> throw RuntimeException("int size mismatch!")
             buf.get().toInt() != CSIZET_SIZE -> throw RuntimeException("size_t size mismatch!")
             buf.get().toInt() != INSTRUCTION_SIZE -> throw RuntimeException("instruction size mismatch!")
@@ -40,78 +42,10 @@ object BinaryChunk {
     }
 
     /**
-     * 从字节流中读取函数原型
-     */
-    private fun readProto(buf: ByteBuffer, parentSource: String = ""): Prototype {
-        var source = readString(buf)
-        if (source.isEmpty()) {
-            source = parentSource
-        }
-
-        return Prototype(
-            source = source,
-            lineDefined = buf.int,
-            lastLineDefined = buf.int,
-            numParams = buf.get(),
-            isVararg = buf.get(),
-            maxStackSize = buf.get(),
-            code = readCode(buf),
-            constants = readConstants(buf),
-            upvalues = readUpvalues(buf),
-            protos = readProtos(buf),
-            lineInfo = readLineInfo(buf),
-            localVars = readLocVars(buf),
-            upvalueNames = readUpvalueNames(buf)
-        )
-    }
-
-    /**
-     * 从字节流中读取指令表
-     */
-    private fun readCode(buf: ByteBuffer): List<Int> {
-        val length = buf.int
-        val code = ArrayList<Int>(length)
-
-        repeat(length) {
-            code.add(buf.int)
-        }
-
-        return code
-    }
-
-    /**
-     * 从字节流中读取常量表
-     */
-    private fun readConstants(buf: ByteBuffer): List<Any?> {
-        val length = buf.int
-        val constants = ArrayList<Any?>(length)
-
-        repeat(length) {
-            constants.add(readConstant(buf))
-        }
-
-        return constants
-    }
-
-    /**
-     * 从字节流中读取常量
-     */
-    private fun readConstant(buf: ByteBuffer): Any? {
-        return when (buf.get().toInt()) {
-            TAG_NIL -> null
-            TAG_BOOLEAN -> buf.get().toInt() != 0
-            TAG_INTEGER -> buf.long
-            TAG_NUMBER -> buf.double
-            TAG_SHORT_STR, TAG_LONG_STR -> readString(buf)
-            else -> throw RuntimeException("corrupted!")
-        }
-    }
-
-    /**
      * 从字节流中读取字符串
      * @return 字符串
      */
-    private fun readString(buf: ByteBuffer): String {
+    fun getString(buf: ByteBuffer): String {
         var size = buf.get().toInt()
 
         if (size == 0) {
@@ -122,102 +56,21 @@ object BinaryChunk {
             size = buf.long.toInt()
         }
 
-        return String(readBytes(buf, size - 1))
-    }
-
-    /**
-     * 从字节流中读取 Upvalue 表
-     */
-    private fun readUpvalues(buf: ByteBuffer): List<Upvalue> {
-        val length = buf.int
-        val upvalues = ArrayList<Upvalue>(length)
-
-        repeat(length) {
-            upvalues.add(
-                Upvalue(
-                    inStack = buf.get(),
-                    idx = buf.get()
-                )
-            )
-        }
-
-        return upvalues
-    }
-
-    /**
-     * 从字节流中读取函数原型表
-     */
-    private fun readProtos(buf: ByteBuffer, parentSource: String = ""): List<Prototype> {
-        val length = buf.int
-        val protos = ArrayList<Prototype>(length)
-
-        repeat(length) {
-            protos.add(readProto(buf, parentSource))
-        }
-
-        return protos
-    }
-
-    /**
-     * 从字节流中读取行号表
-     */
-    private fun readLineInfo(buf: ByteBuffer): List<Int> {
-        val size = buf.int
-        val lineInfo = ArrayList<Int>()
-
-        repeat(size) {
-            lineInfo.add(buf.int)
-        }
-
-        return lineInfo
-    }
-
-    /**
-     * 从字节流中读取局部变量表
-     */
-    private fun readLocVars(buf: ByteBuffer): List<LocVar> {
-        val size = buf.int
-        val locVars = ArrayList<LocVar>(size)
-
-        repeat(size) {
-            locVars.add(
-                LocVar(
-                    varName = readString(buf),
-                    startPC = buf.int,
-                    endPC = buf.int
-                )
-            )
-        }
-
-        return locVars
-    }
-
-    /**
-     * 从字节流中读取 Upvalue 名表
-     */
-    private fun readUpvalueNames(buf: ByteBuffer): List<String> {
-        val size = buf.int
-        val upvalueNames = ArrayList<String>(size)
-
-        repeat(size) {
-            upvalueNames.add(readString(buf))
-        }
-
-        return upvalueNames
+        return String(getBytes(buf, size - 1))
     }
 
     /**
      * 从字节流中读取一个字节
      * @return 一个字节
      */
-    private fun readByte(buf: ByteBuffer) = buf.get()
+    fun getByte(buf: ByteBuffer) = buf.get()
 
     /**
      * 从字节流中读取 n 个字节
      * @param n 想要提取的字节数
      * @return n 个字节
      */
-    private fun readBytes(buf: ByteBuffer, n: Int): ByteArray {
+    fun getBytes(buf: ByteBuffer, n: Int): ByteArray {
         val a = ByteArray(n)
         buf.get(a)
         return a
--- src/main/kotlin/moe/slk/kotlua/binchunk/Prototype.kt
+++ src/main/kotlin/moe/slk/kotlua/binchunk/Prototype.kt
@@ -1,37 +1,169 @@
 package moe.slk.kotlua.binchunk
 
+import java.nio.ByteBuffer
+
 /**
  * 函数原型定义
  *
  * 函数原型主要包含函数基本信息、指令表、常量表、upvalue 表、子函数原型表以及调试信息；基本信息又包括源文件名、起止行号、
  * 固定参数个数、是否是 vararg 函数以及运行函数所必要的寄存器数量；调试信息又包括行号表、局部变量表以及 upvalue 名列表。
  *
- * @param source 源文件名
- * @param lineDefined 函数起始行号
- * @param lastLineDefined 函数休止行号
- * @param numParams 固定参数个数
- * @param isVararg 是否为 Vararg 函数
- * @param maxStackSize 寄存器数量
- * @param code 指令表
- * @param constants 常量表
- * @param upvalues Upvalue 表
- * @param protos 子函数原型表
- * @param lineInfo 行号表
- * @param localVars 局部变量表
- * @param upvalueNames Upvalue 名表
+ * @property source 源文件名
+ * @property lineDefined 函数起始行号
+ * @property lastLineDefined 函数休止行号
+ * @property numParams 固定参数个数
+ * @property isVararg 是否为 Vararg 函数
+ * @property maxStackSize 寄存器数量
+ * @property code 指令表
+ * @property constants 常量表
+ * @property upvalues Upvalue 表
+ * @property protos 子函数原型表
+ * @property lineInfo 行号表
+ * @property localVars 局部变量表
+ * @property upvalueNames Upvalue 名表
  */
-data class Prototype(
-    val source: String,
-    val lineDefined: Int,
-    val lastLineDefined: Int,
-    val numParams: Byte,
-    val isVararg: Byte,
-    val maxStackSize: Byte,
-    val code: List<Int>,
-    val constants: List<Any?>,
-    val upvalues: List<Upvalue>,
-    val protos: List<Prototype>,
-    val lineInfo: List<Int>,
-    val localVars: List<LocVar>,
-    val upvalueNames: List<String>
-)
+class Prototype {
+    var source: String = ""
+    var lineDefined: Int = 0
+    var lastLineDefined: Int = 0
+    var numParams: Byte = 0
+    var isVararg: Byte = 0
+    var maxStackSize: Byte = 0
+    val code: MutableList<Int> = mutableListOf()
+    val constants: MutableList<Any?> = mutableListOf()
+    val upvalues: MutableList<Upvalue> = mutableListOf()
+    val protos: MutableList<Prototype> = mutableListOf()
+    val lineInfo: MutableList<Int> = mutableListOf()
+    val localVars: MutableList<LocVar> = mutableListOf()
+    val upvalueNames: MutableList<String> = mutableListOf()
+
+    /**
+     * 从字节流中读取函数原型
+     *
+     * @param buf 待读取的字节流
+     * @param parentSource 父函数的源文件名
+     */
+    fun read(buf: ByteBuffer, parentSource: String = "") {
+        source = BinaryChunk.getString(buf)
+        if (source.isEmpty()) {
+            source = parentSource
+        }
+        lineDefined = buf.int
+        lastLineDefined = buf.int
+        numParams = buf.get()
+        isVararg = buf.get()
+        maxStackSize = buf.get()
+        readCode(buf)
+        readConstants(buf)
+        readUpvalues(buf)
+        readProtos(buf, source)
+        readLineInfo(buf)
+        readLocVars(buf)
+        readUpvalueNames(buf)
+    }
+
+    /**
+     * 从字节流中读取指令表
+     */
+    private fun readCode(buf: ByteBuffer) {
+        val length = buf.int
+
+        repeat(length) {
+            this.code.add(buf.int)
+        }
+    }
+
+    /**
+     * 从字节流中读取常量表
+     */
+    private fun readConstants(buf: ByteBuffer): List<Any?> {
+        val length = buf.int
+
+        repeat(length) {
+            constants.add(readConstant(buf))
+        }
+
+        return constants
+    }
+
+    /**
+     * 从字节流中读取常量
+     */
+    private fun readConstant(buf: ByteBuffer): Any? {
+        return when (buf.get().toInt()) {
+            TAG_NIL -> null
+            TAG_BOOLEAN -> buf.get().toInt() != 0
+            TAG_INTEGER -> buf.long
+            TAG_NUMBER -> buf.double
+            TAG_SHORT_STR, TAG_LONG_STR -> BinaryChunk.getString(buf)
+            else -> throw RuntimeException("corrupted!")
+        }
+    }
+
+
+    /**
+     * 从字节流中读取 Upvalue 表
+     */
+    private fun readUpvalues(buf: ByteBuffer) {
+        val length = buf.int
+
+        repeat(length) {
+            upvalues.add(
+                Upvalue(
+                    inStack = buf.get(),
+                    idx = buf.get()
+                )
+            )
+        }
+    }
+
+    /**
+     * 从字节流中读取函数原型表
+     */
+    private fun readProtos(buf: ByteBuffer, parentSource: String = "") {
+        val length = buf.int
+
+        repeat(length) {
+            read(buf, parentSource)
+        }
+    }
+
+    /**
+     * 从字节流中读取行号表
+     */
+    private fun readLineInfo(buf: ByteBuffer) {
+        val size = buf.int
+
+        repeat(size) {
+            lineInfo.add(buf.int)
+        }
+    }
+
+    /**
+     * 从字节流中读取局部变量表
+     */
+    private fun readLocVars(buf: ByteBuffer) {
+        val size = buf.int
+
+        repeat(size) {
+            localVars.add(
+                LocVar(
+                    varName = BinaryChunk.getString(buf),
+                    startPC = buf.int,
+                    endPC = buf.int
+                )
+            )
+        }
+    }
+
+    /**
+     * 从字节流中读取 Upvalue 名表
+     */
+    private fun readUpvalueNames(buf: ByteBuffer) {
+        val size = buf.int
+
+        repeat(size) {
+            upvalueNames.add(BinaryChunk.getString(buf))
+        }
+    }
+}
